c830c224b1bd29387cbb9ac075100075c7f51591
retzaf <varasandy567@gmail.com>
2024-11-05 16:00:14 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import kagglehub
 
-# Download latest version
-path = kagglehub.dataset_download("shivamb/netflix-shows")
+df = pd.read_csv("netflix_titles.csv")
 
-print("Path to dataset files:", path)
+st.write("""
+#Netflix Movies
