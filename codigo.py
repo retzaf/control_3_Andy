946915936b49cf0b44e1406ec17e7dc41f9d1d59
retzaf <varasandy567@gmail.com>
2024-11-05 15:32:25 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,3 +4,19 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('/kaggle/input/weather-prediction/seattle-weather.csv')
 df.head()
+
+pred = np.concatenate([train_pred,val_pred,test_pred])
+df_pred = pd.DataFrame(df["temp_max"].copy())
+df_pred.columns=["actual"]
+df_pred = df_pred[WINDOW:]
+df_pred["predicted"] = pred
+
+fig,axes = plt.subplots(2,1,figsize=(14,8),dpi=400)
+
+plt.subplot(2,1,1)
+plt.title("Validation Results")
+sns.lineplot(df_pred[800:],alpha=0.8,palette="flare",linestyle=None);
+
+plt.subplot(2,1,2)
+plt.title("Test Results")
+sns.lineplot(df_pred[1000:],alpha=0.8,palette="flare",linestyle=None);
