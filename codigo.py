8bb591e26cde0c325e5628018f476c667e453715
retzaf <varasandy567@gmail.com>
2024-11-05 16:31:39 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,11 +5,34 @@ db = 'db.csv'
 df = pd.read_csv(db, sep=',', encoding='latin-1') 
 
 st.write("""
-# Marvel vs Dc
-## Gráficos usando la base de datos
+# Top Marvel Movies
+## Gráficos usando la base de datos de Marvel Movies
 """)
 
+st.sidebar.image("Marvel-en-2019-destacada.jpg")
+
 with st.sidebar:
-  st.write('#Opciones')
-  div = st.slider('Número de bins:,0,10,2)
-  st.write("Bins =",div)
+    st.write("# Opciones")
+    div = st.slider('Número de bins:', 0, 10, 2)
+    st.write("Bins =", div)
+
+plt.figure(figsize=(12, 6))
+plt.hist(df['worldwide gross ($m)'], bins=10) # Using a default of 10 bins
+plt.xlabel('worldwide gross ($m)')
+plt.ylabel('% budget recovered')
+plt.title('Histogram of worldwide gross')
+st.pyplot()
+
+plt.figure(figsize=(12, 6))
+plt.hist(df['audience % score'], bins=10)
+plt.xlabel('audience % score')
+plt.ylabel('% budget recovered')
+plt.title('Histogram of Audience Score')
+st.pyplot()
+
+plt.figure(figsize=(12,6))
+plt.hist(df['year'], bins = 10)
+plt.xlabel('year')
+plt.ylabel('')
+plt.title('Histogram of Year')
+st.pyplot()
