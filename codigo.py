18785755eda22e2f177c2c20c8b9fe0fa5b8469b
retzaf <varasandy567@gmail.com>
2024-11-05 16:05:02 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("netflix_titles.csv")
+df = pd.read_csv('netflix_titles.csv')
 
 st.write("""
 # Netflix Movies
