592174122139bdf4b0f3a7c1e1c7afbcc61aacf6
retzaf <varasandy567@gmail.com>
2024-11-05 15:23:22 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,3 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.sidebar.title("insertar texto")
+st.sidebar.header("insertar texto")
+st.sidebar.write("insertar texto")
+
+st.sidebar.image("DEFINICIONES-PNG.jpg")
+if st.sidebar.button("insertar texto"):
+    st.sidebar.write("insertar texto")
+ 
+user_input = st.sidebar.text_input("insertar texto")
+st.sidebar.write("insertar texto", user_input)
