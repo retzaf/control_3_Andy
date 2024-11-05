a7243018d8d130e4b709af1794b0c7798bdcb2dd
retzaf <varasandy567@gmail.com>
2024-11-05 16:59:22 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -12,7 +12,7 @@ st.write("""
 st.sidebar.image("Marvel-Vs.-DC.jpg")
 
 user_input = st.sidebar.text_input("Como te llamas")
-st.sidebar.write("hola user_input)
+st.sidebar.write("hola", user_input)
                  
 with st.sidebar:
     st.write("# Cambiar cantidad de Bins")
